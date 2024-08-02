50e4b939993d778380a50375f2865c6ba72c263f
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-08-01 23:48:56 -0400

    Update DeepFaceVid-v2.py

--- DeepFaceVid-v2.py
+++ DeepFaceVid-v2.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
             #print(results)
             #p.close()
         results=[]
-        for i,frame in enumerate(frame_list):
+        for i,frame in enumerate(frame_list[:12]):
             results.append(get_potraits(frame))
             st.write(f"...frame {i} processed")
             
